78bccb005547240d610876bc09025b7d899bd7c3
OmarElgammal1 <omarelgammal2004@gmail.com>
2023-10-30 18:32:10 +0200

    Added Validation Data and Prediction

--- model.py
+++ model.py
@@ -6,8 +6,12 @@ from sklearn.metrics import accuracy_score
 # Load the iris dataset
 iris = load_iris()
 
-# Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.3, random_state=42)
+X = iris.data
+y = iris.target
+
+# Split the dataset into training and testing sets Train: 40%, Validation:30%, Test:30%
+X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.6, random_state=42)
+X_validation, X_test, y_validation, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
 
 # Gaussian Naive Bayes classifier
 naiveBayes = GaussianNB()
@@ -15,10 +19,16 @@ naiveBayes = GaussianNB()
 # Train the classifier on the training data
 naiveBayes.fit(X_train, y_train)
 
+# Use the classifier to make predictions on the validation data
+y_validation_pred = naiveBayes.predict(X_validation)
+
+# Calculate accuracy on the validation data
+accuracy_validation = accuracy_score(y_validation, y_validation_pred)
+print("Validation Accuracy:", accuracy_validation)
+
 # Use the classifier to make predictions on the testing data
-y_pred = naiveBayes.predict(X_test)
+y_test_pred = naiveBayes.predict(X_test)
 
 # Calculate the accuracy of the classifier
-accuracy = accuracy_score(y_test, y_pred)
-
+accuracy = accuracy_score(y_test, y_test_pred)
 print("Accuracy:", accuracy)
