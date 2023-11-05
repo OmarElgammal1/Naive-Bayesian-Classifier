d709a0f922c2a2ac2581ee5f2add2f38af029fdc
OmarElgammal1 <omarelgammal2004@gmail.com>
2023-11-05 20:19:12 +0200

    Fixed train_validate_test_split (returned data type)

--- utils.py
+++ utils.py
@@ -1,32 +1,36 @@
 import numpy as np
 
 # Split the dataset into training and testing sets Train: 40%, Validation:30%, Test:30%
-def train_validate_test_split (data, labels, testRatio =0.3, valRatio =0.3, random_state=0):
-    if testRatio < 0 or valRatio < 0 or testRatio + valRatio >=1:
+def train_validate_test_split (data, labels, test_ratio =0.3, val_ratio =0, random_state=0):
+    if test_ratio < 0 or val_ratio < 0 or test_ratio + val_ratio >=1:
         raise ValueError("Invalid test and validation ratio values")
     
     # Calculate the sizes of each split
     total_samples = len(data)
-    test_size = int(testRatio * total_samples)
-    val_size = int(valRatio * total_samples)
+    test_size = int(test_ratio * total_samples)
+    val_size = int(val_ratio * total_samples)
     train_size = total_samples - test_size - val_size
     
     # Randomize based on random_state
     if(random_state):
         np.random.seed(random_state)
 
+
+    # Shuffle the data and the labels
+    indices = np.arange(total_samples)
+    np.random.shuffle(indices)
+
+    # Split the data and labels
+    test_indices = indices[:test_size]
+    val_indices = indices[test_size:test_size + val_size]
+    train_indices = indices[test_size + val_size:]
+
+    train_data, val_data, test_data = data[train_indices], data[val_indices], data[test_indices]
+    train_labels, val_labels, test_labels = labels[train_indices], labels[val_indices], labels[test_indices]
     # Shuffle the data and the labels
     combined = list(zip(data, labels))
     np.random.shuffle(combined)
     data, labels = zip(*combined)
-    
-    # Split the data and labels
-    train_data = data[:train_size] 
-    val_data = data[train_size:train_size + val_size]
-    test_data = data[train_size + val_size:]
-    train_labels = labels[:train_size] 
-    val_labels = labels[train_size:train_size + val_size] 
-    test_labels = labels[train_size + val_size:]
     return train_data, val_data, test_data, train_labels, val_labels, test_labels
 
 
